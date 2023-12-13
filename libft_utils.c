5d1cc9942e1612ed46b619565428a3665269b506
Yacoub <yaharkat@student-1337.ma>
2023-12-13 14:55:01 +0100

    initialization of a basic structure to start the project

--- libft_utils.c
+++ libft_utils.c
@@ -14,9 +14,9 @@
 
 int	ft_atol(const char *nptr)
 {
-	size_t i;
-	long long number;
-	int sign;
+	size_t		i;
+	long long	number;
+	int			sign;
 
 	i = 0;
 	sign = 1;
@@ -32,4 +32,94 @@ int	ft_atol(const char *nptr)
 	while (ft_isdigit(nptr[i]))
 		number = number * 10 + nptr[i++] - 48;
 	return (number * sign);
+}
+
+size_t	ft_countword(char const *s, char c)
+{
+	size_t	i;
+	size_t	old_i;
+	size_t	count;
+
+	i = 0;
+	old_i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		old_i = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (old_i < i)
+			count++;
+	}
+	return (count);
+}
+
+size_t	compute_w_len(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s && *s == c)
+		s++;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
+
+char	**free_strs(char **strs, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= size)
+		free(strs[i++]);
+	free(strs);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	words_count;
+	size_t	i;
+	size_t	cpd_w;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	cpd_w = 0;
+	words_count = ft_countword(s, c);
+	split = (char **)malloc(sizeof(char *) * (words_count + 1));
+	if (!split)
+		return (NULL);
+	split[words_count] = NULL;
+	while (s[i] && cpd_w < words_count)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		split[cpd_w] = ft_substr(s, i, compute_w_len(s + i, c));
+		if (!split[cpd_w++])
+			return (free_strs(split, words_count));
+		i += compute_w_len(s + i, c);
+	}
+	return (split);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0 && dst && src)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
 }
--- libft_utils_2.c
+++ libft_utils_2.c
@@ -11,3 +11,46 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, &s[start], (len + 1));
+	return (sub);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*dest;
+
+	len = ft_strlen(s);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s, (len + 1));
+	return (dest);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
