04b95335d1dbe49786ab9a5695ee2c6f690b8a48
Yacoub <yaharkat@student.1337.com>
2023-12-31 23:37:43 +0100

    feat: added libft and join args

--- push_swap.c
+++ push_swap.c
@@ -6,23 +6,42 @@
 /*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:09:16 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/12/13 12:09:16 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/12/31 23:35:54 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+char **join_args(int ac, char **av)
 {
-	t_stack_node	*a;
-	t_stack_node	*b;
+	int i;
+
+	i = 0;
+	while (++i < ac)
+	{
+		if (!av[i])
+		{
+			ft_putstr_fd("Error\n", 2);
+			if (i > 2)
+				free(av[1]);
+			exit(EXIT_FAILURE);
+		}
+		av[1] = ft_strjoin(av[1], " ");
+		av[1] = ft_strjoin(av[1], av[i]);
+	}
+	return (ft_split(av[1], ' '));
+}
+
+int main(int ac, char **av)
+{
+	t_stack_node *a;
+	t_stack_node *b;
 
 	a = NULL;
 	b = NULL;
-	if (ac < 2 || (ac == 2 && !av[1][0]))
+	if (ac < 2)
 		return (EXIT_FAILURE);
-	if (ac == 2)
-		av = ft_split(av[1], ' ');
+	av = join_args(ac, av);
 	init_stack(&a, av, ac == 2);
 	if (!is_stack_sorted(a))
 	{
